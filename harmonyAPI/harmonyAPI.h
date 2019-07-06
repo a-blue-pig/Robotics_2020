4da85ac97e7e2ad857ff784d406ed5999e8d43ec
Steven <stevencarlos99@outlook.com>
2019-07-06 15:54:26 -0600

    adding files

---
+++ harmonyAPI/harmonyAPI.h
@@ -0,0 +1,12 @@
+#ifndef HARMONYAPI_INCLUDED
+#define HARMONYAPI_INCLUDED
+
+#ifdef USING_ODOMETRY
+  #include "Modules/Odometry/odometry.cpp"
+#endif
+
+#ifdef  USING_SETUP
+  #include "Modules/Setup/setup.hpp"
+#endif
+
+#endif
